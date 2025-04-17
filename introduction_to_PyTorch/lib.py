c94f9cade2a4221e7f11f1795564c27eaec32e3b
lixunhuan1899 <xunhuanli67@gmail.com>
2025-04-17 18:24:15 +0800

    first_test

--- introduction_to_PyTorch/lib.py
+++ introduction_to_PyTorch/lib.py
@@ -3,3 +3,20 @@ import math
 import numpy as np
 import time
 
+import matplotlib.pyplot as plt
+
+#Ipython为notebook环境而设置
+#from IPython.display import set_matplotlib_formats
+
+from matplotlib.colors import to_rgba
+import seaborn as sns
+sns.set()
+
+#from tqdm.notebook import tqdm
+
+from tqdm import tqdm
+
+import torch
+
+print("Using torch", torch.__version__)
+
